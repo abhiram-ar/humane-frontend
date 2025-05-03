6abb3057dbe51c6def12ed0e534e52862947fcda
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-03 13:12:57 +0530

    tailwind prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -24,11 +24,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="text-4xl">
+      <p className="text-4xl hover:text-5xl">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

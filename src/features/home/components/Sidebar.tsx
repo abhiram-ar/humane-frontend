582fdb3792764339ba74fd214fba8cb3bdd3c158
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-10 21:34:57 +0530

    logout animated sectio

--- src/features/home/components/Sidebar.tsx
+++ src/features/home/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import { Bell, Home, LucideIcon, MessageSquareText, Settings, User, UserRound } 
 import { NavLink } from "react-router";
 import humanelogo from "@/assets/humaneSegoeScriptBold.svg";
 import CreatePostButton from "./CreatePostButton";
+import UserLogout from "@/features/profile/components/UserLogout";
 
 type SidebarCongfig = {
   name: string;
@@ -20,31 +21,39 @@ const config: SidebarCongfig[] = [
 
 const Sidebar = () => {
   return (
-    <div className="bg-grey fixed h-screen w-90 text-white">
-      <div className="text-pop-green mb-5 px-12 py-8 text-2xl font-bold">
+    <div className="bg-grey fixed flex h-screen w-90 flex-col justify-between text-white">
+      <div className="text-pop-green px-12 pt-8 text-2xl font-bold">
         <img src={humanelogo} alt="" />
       </div>
-      <div className="me-5">
-        <div>
-          {config.map((data, index) => (
-            <div key={index}>
-              <NavLink
-                to={data.path}
-                className={({ isActive }) =>
-                  `my-3 flex items-center gap-3 rounded-e-2xl px-10 py-3 text-xl transition-all duration-500 ease-out ${
-                    isActive ? "bg-grey-light font-semibold" : "hover:bg-zinc-700/50"
-                  }`
-                }
-              >
-                <data.icon />
-                {data.name}
-              </NavLink>
-            </div>
-          ))}
+
+      <div className="-mt-20">
+        <div className="me-5">
+          <div>
+            {config.map((data, index) => (
+              <div key={index}>
+                <NavLink
+                  to={data.path}
+                  className={({ isActive }) =>
+                    `my-3 flex items-center gap-3 rounded-e-2xl px-10 py-3 text-xl transition-all duration-500 ease-out ${
+                      isActive ? "bg-grey-light font-semibold" : "hover:bg-zinc-700/50"
+                    }`
+                  }
+                >
+                  <data.icon />
+                  {data.name}
+                </NavLink>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="mt-10 flex justify-center">
+          <CreatePostButton />
         </div>
       </div>
-      <div className="flex justify-center mt-10">
-        <CreatePostButton />
+
+      <div className="mb-3 me-5">
+        <UserLogout />
       </div>
     </div>
   );

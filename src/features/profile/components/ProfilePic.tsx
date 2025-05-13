57c6edc73b753a817179cc95fd7843b775c2f3b2
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-13 16:26:46 +0530

    profile pic shimmer

--- src/features/profile/components/ProfilePic.tsx
+++ src/features/profile/components/ProfilePic.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 type Props = {
   url: string;
 };
 const ProfilePic: React.FC<Props> = ({ url }) => {
+  const [isLoading, setIsLoading] = useState(false);
   return (
-    <div className="border-grey-light h-52 w-52 overflow-hidden rounded-full border-3 bg-zinc-400">
-      <img src={url} className="h-full w-full object-cover" alt="" />
+    <div className="border-grey-light relative h-52 w-52 overflow-hidden rounded-full border-3 bg-zinc-500">
+      {isLoading && <div className="absolute inset-0 animate-pulse bg-zinc-400"></div>}
+      <img
+        src={url}
+        onLoadStart={() => setIsLoading(true)}
+        onLoad={() => setIsLoading(false)}
+        className={`h-full w-full object-cover transition-all duration-0 ease-in ${isLoading ? "opacity-0" : "opacity-100"} `}
+        alt=""
+      />
     </div>
   );
 };

a205f76f610e3672f2a25408a01a559c5dc5dccf
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-13 14:47:32 +0530

    cover loading shimmer

--- src/features/profile/components/CoverPhoto.tsx
+++ src/features/profile/components/CoverPhoto.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
   url: string;
 };
 const CoverPhoto: React.FC<Props> = ({ url }) => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
-    <div className="h-60 w-full bg-zinc-400 overflow-clip">
-      <img className="h-full w-full object-cover" src={url} alt="" />
+    <div className="h-60 w-full overflow-clip bg-zinc-500">
+      {isLoading && <div className={`absolute inset-0 animate-pulse bg-zinc-600`}></div>}
+
+      <img
+        className={`h-full w-full object-cover transition-all duration-500 ease-out ${isLoading ? "opacity-0 scale-105" : "opacity-100 scale-100"} `}
+        src={url}
+        alt="cover photo"
+        onLoad={() => setIsLoading(false)}
+      />
     </div>
   );
 };

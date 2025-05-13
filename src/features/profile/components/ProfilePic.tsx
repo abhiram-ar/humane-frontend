4deef783ddf58d4401e84b3dcc8bb0f3e87e956f
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-13 16:45:12 +0530

    animation fix

--- src/features/profile/components/ProfilePic.tsx
+++ src/features/profile/components/ProfilePic.tsx
@@ -4,15 +4,14 @@ type Props = {
   url: string;
 };
 const ProfilePic: React.FC<Props> = ({ url }) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <div className="border-grey-light relative h-52 w-52 overflow-hidden rounded-full border-3 bg-zinc-500">
-      {isLoading && <div className="absolute inset-0 animate-pulse bg-zinc-400"></div>}
+      {isLoading && <div className="absolute inset-0 animate-pulse bg-zinc-400/20"></div>}
       <img
         src={url}
-        onLoadStart={() => setIsLoading(true)}
         onLoad={() => setIsLoading(false)}
-        className={`h-full w-full object-cover transition-all duration-0 ease-in ${isLoading ? "opacity-0" : "opacity-100"} `}
+        className={`h-full w-full object-cover transition-all duration-100 ease-in ${isLoading ? "scale-102 opacity-0" : "scale-100 opacity-100"} `}
         alt=""
       />
     </div>

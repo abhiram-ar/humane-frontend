13f89af695e43f7ced5b58e38e8daf14a165b68c
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-24 01:25:09 +0530

    working live search

--- src/features/search/components/SearchUserBar.tsx
+++ src/features/search/components/SearchUserBar.tsx
@@ -1,11 +1,18 @@
 import { Search } from "lucide-react";
+import { useState } from "react";
 
-const SearchUserBar = () => {
+type Props = {
+  state: ReturnType<typeof useState<string>>;
+};
+
+const SearchUserBar: React.FC<Props> = ({ state: [query, setQuery] }) => {
   return (
-    <div className="bg-grey-light group w-full flex h-25 items-center justify-center gap-2 px-5 py-2">
-      <Search className="text-zinc-300 group-focus-within:text-pop-green transition-all duration-100 ease-in group-focus-within:opacity-100" />
+    <div className="bg-grey-light group flex h-25 w-full items-center justify-center gap-2 px-5 py-2">
+      <Search className="group-focus-within:text-pop-green text-zinc-300 transition-all duration-100 ease-in group-focus-within:opacity-100" />
       <input
         type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
         className="w-full text-lg text-white focus:outline-none"
         placeholder="search user"
       />
--- src/features/search/pages/SearchPage.tsx
+++ src/features/search/pages/SearchPage.tsx
@@ -1,15 +1,17 @@
-import { Fragment, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SearchUserBar from "../components/SearchUserBar";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { InfiniteScrollResponse } from "../Types/SearchResult";
 import { api } from "@/lib/axios";
 import UserListItem from "../components/UserListItem";
 import { Link } from "react-router";
+import { Loader } from "lucide-react";
 
 const SearchPage = () => {
-  const [query, setQuery] = useState<string | null>("emma");
+  const [query, setQuery] = useState<string | undefined>("emma");
+  const observerRef = useRef<HTMLDivElement | null>(null);
 
-  const { data, fetchNextPage } = useInfiniteQuery({
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
     queryKey: ["find-users", query],
     queryFn: async ({ pageParam }) => {
       const queryParams =
@@ -30,28 +32,56 @@ const SearchPage = () => {
       return res.data.data;
     },
     initialPageParam: 0,
-    getNextPageParam: (lastPage) => lastPage.scroll.cursor ?? undefined,
+    // note: in our api, if send the request after the last page, cusor will be null
+    // returing undefined makes the hasNestPage turn false
+    getNextPageParam: (lastPage) => (lastPage.scroll.hasMore ? lastPage.scroll.cursor : undefined),
   });
 
-  console.log(data);
+  useEffect(() => {
+    if (!observerRef.current) {
+      return;
+    }
+    const observer = new IntersectionObserver((entry) => {
+      // make sure we dont fetch the page when a request is in fligh
+      if (entry[0].isIntersecting && hasNextPage && !isFetching) {
+        fetchNextPage();
+      }
+    });
+    observer.observe(observerRef.current);
+
+    return () => observer.disconnect();
+  });
+
+  console.log("hasnext", hasNextPage);
   return (
-    <div className="w-120 border-x border-zinc-400/50">
+    <div className="min-h-full w-120 border-x border-zinc-400/50">
       <div className="sticky top-0 z-20">
-        <SearchUserBar />
+        <SearchUserBar state={[query, setQuery]} />
       </div>
 
-      <div className="p-5 text-lg text-white">
+      <div className="px-5 pt-5 text-lg text-white">
         {data?.pages
           .flatMap((page) => [...page.users])
           .map((user) => (
             <Link key={user.id} to={`/user/${user.id}`}>
               <UserListItem
                 profileURL={user.avatarURL}
-                userName={`${user.firstName + (user.lastName || "")}`}
+                userName={`${user.firstName} ${user.lastName || ""}`}
               />
             </Link>
           ))}
-        <div onClick={() => fetchNextPage()}>end</div>
+      </div>
+
+      <div ref={observerRef} />
+
+      <div className="pb-5">
+        {hasNextPage && isFetching ? (
+          <div className="flex justify-center">
+            <Loader className="text-pop-green/75 animate-spin" />
+          </div>
+        ) : (
+          <p className="text-center text-sm text-zinc-400">No more matches</p>
+        )}
       </div>
     </div>
   );

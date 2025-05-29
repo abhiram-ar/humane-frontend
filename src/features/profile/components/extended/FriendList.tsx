cb07d273bf1dbe1fac562cb1247d7e735af844c6
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-29 15:54:03 +0530

    refacor friendslist

--- src/features/profile/components/extended/FriendList.tsx
+++ src/features/profile/components/extended/FriendList.tsx
@@ -1,35 +1,13 @@
 import Spinner from "@/components/Spinner";
 import UserListItem from "@/features/search/components/UserListItem";
-import { api } from "@/lib/axios";
-import {
-  UserListInfinityScollParams,
-  UserListInfinityScollQueryParams,
-} from "@/types/UserInfinitryScrollParams.type";
-import { useInfiniteQuery } from "@tanstack/react-query";
 import React, { useEffect, useRef } from "react";
 import { Link } from "react-router";
-
-// TODO: reafacor
-export type FriendList = {
-  id: string;
-  firstName: string;
-  lastName?: string | null;
-  status: "PENDING" | "ACCEPTED";
-  avatarURL: string | null;
-}[];
-
-export type GetFriendsListResponse = {
-  success: boolean;
-  message: string;
-  data: {
-    friends: FriendList;
-    from: UserListInfinityScollParams;
-  };
-};
+import useFriendsListInfniteQuery from "../../hooks/useFriendsListInfniteQuery";
 
 type Props = {
   userId: string;
 };
+
 const FriendList: React.FC<Props> = ({ userId }) => {
   const observerRef = useRef<HTMLDivElement | null>(null);
 
@@ -39,26 +17,7 @@ const FriendList: React.FC<Props> = ({ userId }) => {
     isFetching,
     hasNextPage,
     fetchNextPage,
-  } = useInfiniteQuery({
-    queryKey: ["user-friends", "list"],
-    queryFn: async ({ pageParam }) => {
-      const queryParams: UserListInfinityScollQueryParams & { targetUserId: string } = {
-        size: 9,
-        targetUserId: userId,
-      };
-
-      if (pageParam.createdAt !== "ini" && pageParam.lastId !== "ini") {
-        queryParams.createdAt = pageParam?.createdAt;
-        queryParams.lastId = pageParam?.lastId;
-      }
-      const res = await api.get<GetFriendsListResponse>("/api/v1/user/social/friend", {
-        params: queryParams,
-      });
-      return res.data.data;
-    },
-    initialPageParam: { lastId: "ini", createdAt: "ini" },
-    getNextPageParam: (lastPage) => (lastPage.from?.hasMore ? lastPage.from : null),
-  });
+  } = useFriendsListInfniteQuery(userId);
 
   useEffect(() => {
     if (!observerRef.current) return;
@@ -74,11 +33,11 @@ const FriendList: React.FC<Props> = ({ userId }) => {
   }, [fetchNextPage, hasNextPage, isFetching]);
 
   return (
-    <div className="h-100 overflow-y-auto py-1 text-white">
+    <div className="h-100 overflow-y-auto pb-1 text-white">
       {infiniteData &&
         infiniteData?.pages?.map((page) =>
           page?.friends?.map((user) => (
-            <div key={user.id} className="mx-2 flex justify-between">
+            <div key={user.id} className="flex justify-between">
               <Link to={`/user/${user.id}`} target="">
                 <UserListItem
                   profileURL={user.avatarURL}
---
+++ src/features/profile/hooks/useFriendsListInfniteQuery.tsx
@@ -0,0 +1,48 @@
+import { api } from "@/lib/axios";
+import {
+  UserListInfinityScollParams,
+  UserListInfinityScollQueryParams,
+} from "@/types/UserInfinitryScrollParams.type";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+export type FriendList = {
+  id: string;
+  firstName: string;
+  lastName?: string | null;
+  status: "PENDING" | "ACCEPTED";
+  avatarURL: string | null;
+}[];
+
+export type GetFriendsListResponse = {
+  success: boolean;
+  message: string;
+  data: {
+    friends: FriendList;
+    from: UserListInfinityScollParams;
+  };
+};
+
+const useFriendsListInfniteQuery = (userId: string) => {
+  return useInfiniteQuery({
+    queryKey: ["user-friends", "list"],
+    queryFn: async ({ pageParam }) => {
+      const queryParams: UserListInfinityScollQueryParams & { targetUserId: string } = {
+        size: 9,
+        targetUserId: userId,
+      };
+
+      if (pageParam.createdAt !== "ini" && pageParam.lastId !== "ini") {
+        queryParams.createdAt = pageParam?.createdAt;
+        queryParams.lastId = pageParam?.lastId;
+      }
+      const res = await api.get<GetFriendsListResponse>("/api/v1/user/social/friend", {
+        params: queryParams,
+      });
+      return res.data.data;
+    },
+    initialPageParam: { lastId: "ini", createdAt: "ini" },
+    getNextPageParam: (lastPage) => (lastPage.from?.hasMore ? lastPage.from : null),
+  });
+};
+
+export default useFriendsListInfniteQuery;

2377b60305ba6632e5fb04c795e43574678b4cad
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-13 17:43:48 +0530

    poster loader

--- src/features/home/components/PosterImage.tsx
+++ src/features/home/components/PosterImage.tsx
@@ -1,3 +1,4 @@
+import { ImageOff } from "lucide-react";
 import React, { ComponentPropsWithRef, useState } from "react";
 
 const PosterImage: React.FC<ComponentPropsWithRef<"div"> & { url: string }> = ({
@@ -5,7 +6,10 @@ const PosterImage: React.FC<ComponentPropsWithRef<"div"> & { url: string }> = ({
   className,
   ...props
 }) => {
-  const [orientation, SetOrientation] = useState<"landscape" | "potrait">("landscape");
+  const [orientation, SetOrientation] = useState<"landscape" | "potrait" | "unknown">("potrait");
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
   const handleOnload: React.DOMAttributes<HTMLImageElement>["onLoad"] = (e) => {
     const { naturalHeight, naturalWidth } = e.target as HTMLImageElement;
     if (naturalHeight > naturalWidth) {
@@ -13,17 +17,35 @@ const PosterImage: React.FC<ComponentPropsWithRef<"div"> & { url: string }> = ({
     } else {
       SetOrientation("landscape");
     }
+    setIsLoading(false);
   };
 
   return (
     <div
-      className={`aspect-auto overflow-clip rounded-xl w-fit ${orientation === "potrait" ? "max-h-120 " : ""} ${className}`}
+      className={`relative aspect-auto overflow-clip rounded-xl ${orientation === "potrait" ? "max-h-110 w-fit" : ""} ${className} ${isError ? "h-110 w-full" : ""}`}
       {...props}
     >
+      {isLoading && url && (
+        <div className="absolute inset-0 z-10 h-full w-full animate-pulse rounded-2xl border border-zinc-400/50 bg-zinc-400/50"></div>
+      )}
+
+      {(isError || !url) && (
+        <div
+          onLoad={() => setIsLoading(false)}
+          className={`animate-in absolute inset-0 z-10 flex h-full w-full scale-110 items-center justify-center bg-zinc-500 text-white/30 duration-[2000]`}
+        >
+          <ImageOff className="text-white" size={40} />
+        </div>
+      )}
+
       <img
         onLoad={handleOnload}
-        className={`aspect-auto h-full w-full object-cover ${orientation === "potrait" ? "max-h-120" : ""}`}
+        className={`aspect-auto h-full w-full object-cover ${isLoading ? "scale-102 opacity-0" : "scale-100 opacity-100"} ${orientation === "potrait" ? "max-h-110" : ""}`}
         src={url}
+        onError={() => {
+          setIsLoading(false);
+          setIsError(true);
+        }}
         alt=""
       />
     </div>

ef9638ca1aae5636441052d57c1196a71df4f405
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-19 20:44:08 +0530

    notificatio apis

--- src/lib/API_ROUTES.ts
+++ src/lib/API_ROUTES.ts
@@ -1,5 +1,6 @@
 export const API_ROUTES = {
   QUERY_SERVICE: "/api/v1/query",
   FEED_SERVICE: "/api/v1/feed",
-  POST_SERVICE: "/api/v1/post"
+  POST_SERVICE: "/api/v1/post",
+  NOTIFICATION_SERVICE: "/api/v1/notification",
 } as const;

c9c38afba288ae62d5d7ffce63dc647ae49ddd69
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-21 23:59:39 +0530

    refactoed comment like

--- src/features/home/components/CommentLike.tsx
+++ src/features/home/components/CommentLike.tsx
@@ -1,11 +1,6 @@
-import { API_ROUTES } from "@/lib/API_ROUTES";
-import { api } from "@/lib/axios";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Heart } from "lucide-react";
 import React from "react";
-import { InfintiteCommentsData } from "../types/InfiniteCommentData";
-import { produce } from "immer";
-import useUserId from "@/features/profile/hooks/useUserId";
+import useLikeCommentMutation from "../hooks/useLikeCommentMutation";
 
 type Props = {
   postId: string;
@@ -15,65 +10,18 @@ type Props = {
   hasPostAuthorLiked?: boolean;
 };
 
-type LikeCommentResponse = {
-  data: {
-    like: {
-      id: string;
-      authorId: string;
-      commentId: string;
-    };
-  };
-};
-
 const CommentLike: React.FC<Props> = ({ likeCount, hasLikedByUser, commentId, postId }) => {
-  const queryClient = useQueryClient();
-  const authenticatedUserId = useUserId();
+  const { mutate: likeComment } = useLikeCommentMutation(postId, commentId);
 
-  const { mutate } = useMutation({
-    mutationFn: async (commentId: string) => {
-      const res = await api.post<LikeCommentResponse>(
-        `${API_ROUTES.POST_SERVICE}/comment/${commentId}/like`,
-      );
-      return res.data.data;
-    },
-  });
-
-  const handleLikeClick = () => {
-    mutate(commentId, {
-      onSuccess: (responseData) => {
-        queryClient.setQueryData(["comments", postId], (oldData: InfintiteCommentsData) => {
-          if (!oldData) return oldData;
-
-          const newPages: NonNullable<InfintiteCommentsData>["pages"] = oldData.pages
-            ? produce(oldData.pages, (draftPage) => {
-                draftPage.forEach((page) =>
-                  page.comments.forEach((comment) => {
-                    if (comment.id === responseData.like.commentId) {
-                      if (typeof comment.likeCount === "number" && !comment.hasLikedByUser) {
-                        comment.likeCount++;
-                      }
-                      if (authenticatedUserId === responseData.like.authorId) {
-                        comment.hasLikedByUser = true;
-                      }
-                    }
-                  }),
-                );
-              })
-            : [];
-
-          console.log("newpages", newPages);
-
-          return { pageParams: oldData.pageParams, pages: newPages } as InfintiteCommentsData;
-        });
-      },
-    });
+  const handleUnlike = () => {
+    console.log("not implemented");
   };
 
   return (
     <div className="flex flex-col items-center justify-center text-white">
       <div
         className="cursor-pointer rounded-full p-1 hover:bg-zinc-400/50"
-        onClick={() => handleLikeClick()}
+        onClick={() => (hasLikedByUser ? handleUnlike() : likeComment(commentId))}
       >
         <Heart
           size={15}
---
+++ src/features/home/hooks/useLikeCommentMutation.tsx
@@ -0,0 +1,57 @@
+import { API_ROUTES } from "@/lib/API_ROUTES";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InfintiteCommentsData } from "../types/InfiniteCommentData";
+import useUserId from "@/features/profile/hooks/useUserId";
+import { api } from "@/lib/axios";
+import { produce } from "immer";
+
+type LikeCommentResponse = {
+  data: {
+    like: {
+      id: string;
+      authorId: string;
+      commentId: string;
+    };
+  };
+};
+
+const useLikeCommentMutation = (postId: string, commentId: string) => {
+  const authenticatedUserId = useUserId();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ["likeComment", commentId],
+    mutationFn: async (commentId: string) => {
+      const res = await api.post<LikeCommentResponse>(
+        `${API_ROUTES.POST_SERVICE}/comment/${commentId}/like`,
+      );
+      return res.data.data;
+    },
+    onSuccess: (responseData) => {
+      queryClient.setQueryData(["comments", postId], (oldData: InfintiteCommentsData) => {
+        if (!oldData) return oldData;
+
+        const newPages: NonNullable<InfintiteCommentsData>["pages"] = oldData.pages
+          ? produce(oldData.pages, (draftPage) => {
+              draftPage.forEach((page) =>
+                page.comments.forEach((comment) => {
+                  if (comment.id === responseData.like.commentId) {
+                    if (typeof comment.likeCount === "number" && !comment.hasLikedByUser) {
+                      comment.likeCount++;
+                    }
+                    if (authenticatedUserId === responseData.like.authorId) {
+                      comment.hasLikedByUser = true;
+                    }
+                  }
+                }),
+              );
+            })
+          : [];
+
+        return { pageParams: oldData.pageParams, pages: newPages } as InfintiteCommentsData;
+      });
+    },
+  });
+};
+
+export default useLikeCommentMutation;

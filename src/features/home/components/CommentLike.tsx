46e72c9e120060dd9c8c397598a01351b365fde7
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-23 14:51:41 +0530

    refactored comment dislike

--- src/features/home/components/CommentLike.tsx
+++ src/features/home/components/CommentLike.tsx
@@ -1,12 +1,7 @@
 import { Heart } from "lucide-react";
 import React from "react";
 import useLikeCommentMutation from "../hooks/useLikeCommentMutation";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { api } from "@/lib/axios";
-import { API_ROUTES } from "@/lib/API_ROUTES";
-import { InfintiteCommentsData } from "../types/InfiniteCommentData";
-import { produce } from "immer";
-import useUserId from "@/features/profile/hooks/useUserId";
+import useUnlikeCommentMutation from "../hooks/useUnlikeCommentMutation";
 
 type Props = {
   postId: string;
@@ -16,54 +11,9 @@ type Props = {
   hasPostAuthorLiked?: boolean;
 };
 
-type UnlikeCommentResponse = {
-  data: {
-    like: {
-      id: string;
-      authorId: string;
-      commentId: string;
-    };
-  };
-};
-
 const CommentLike: React.FC<Props> = ({ likeCount, hasLikedByUser, commentId, postId }) => {
   const { mutate: likeComment } = useLikeCommentMutation(postId, commentId);
-  const queryClient = useQueryClient();
-  const authenticatedUserId = useUserId();
-
-  const { mutate: unlikeComment } = useMutation({
-    mutationKey: ["unlikeComment", commentId],
-    mutationFn: async (commentId: string) => {
-      const res = await api.post<UnlikeCommentResponse>(
-        `${API_ROUTES.POST_SERVICE}/comment/${commentId}/unlike`,
-      );
-      return res.data.data;
-    },
-    onSuccess: (responseData) => {
-      queryClient.setQueryData(["comments", postId], (oldData: InfintiteCommentsData) => {
-        if (!oldData) return oldData;
-
-        const newPages: NonNullable<InfintiteCommentsData>["pages"] = oldData.pages
-          ? produce(oldData.pages, (draftPage) => {
-              draftPage.forEach((page) =>
-                page.comments.forEach((comment) => {
-                  if (comment.id === responseData.like.commentId) {
-                    if (typeof comment.likeCount === "number" && comment.hasLikedByUser) {
-                      comment.likeCount--;
-                    }
-                    if (authenticatedUserId === responseData.like.authorId) {
-                      comment.hasLikedByUser = false;
-                    }
-                  }
-                }),
-              );
-            })
-          : [];
-
-        return { pageParams: oldData.pageParams, pages: newPages } as InfintiteCommentsData;
-      });
-    },
-  });
+  const { mutate: unlikeComment } = useUnlikeCommentMutation(postId, commentId);
 
   return (
     <div className="flex flex-col items-center justify-center text-white">

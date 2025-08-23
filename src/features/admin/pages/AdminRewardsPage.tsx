36d1f61c421a79609d221bd1dccd5f4a292d5c81
abhiram-ar <abhiram2001.ars@gmail.com>
2025-08-23 21:56:38 +0530

    reward mangement

--- src/features/admin/pages/AdminRewardsPage.tsx
+++ src/features/admin/pages/AdminRewardsPage.tsx
@@ -1,20 +1,33 @@
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/Table";
 import RewardOverviewContainer from "../components/RewardOverviewContainer";
 import TableRowShimmer from "../components/TableRowShimmer";
-import { useUserListQuery } from "../hooks/useUserListQuery";
 import { useState } from "react";
 import { IQueryFilter } from "../types/QueryFilter";
 import TableNoItemRow from "../components/TableNoItemRow";
 import Pagination from "../components/Pagination";
-import UserManagementRow from "../components/UserManagementRow";
 import EditRewardConfigButton from "../components/EditRewardConfigButton";
+import TableSearch from "../components/TableSearch";
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/lib/axios";
+import { GetUsersScoreResponse } from "../types/GetUserRewardListResponse";
+import UserReardTableRow from "../components/UserRewardTableRow";
+import { API_ROUTES } from "@/lib/API_ROUTES";
 
 const AdminRewardsPage = () => {
   const [filter, setFilter] = useState<IQueryFilter>({ search: "", page: 1, limit: 9 });
 
-  const { data, isLoading, isError } = useUserListQuery(filter);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["admin-user-rewards-list", { search: filter.search, page: filter.page }],
+    queryFn: async () => {
+      const response = await api.get<GetUsersScoreResponse>(`${API_ROUTES.REWARD_ROUTE}/list`, {
+        params: { search: filter.search, page: filter.page, limit: filter.limit },
+      });
+      return response.data.data;
+    },
 
-  const handleToogleBlock = () => {};
+    staleTime: 60 * 1000,
+    refetchOnMount: false,
+  });
   return (
     <div>
       <h2 className="text-almost-white mb-5 font-sans text-2xl font-semibold">
@@ -25,26 +38,22 @@ const AdminRewardsPage = () => {
         <RewardOverviewContainer />
       </div>
 
-      <div className="mb-2 flex justify-end">
+      <div className="mb-2 flex justify-between">
+        <TableSearch search={filter.search} setFilter={setFilter} />
         <EditRewardConfigButton />
       </div>
       <Table>
         <TableHeader>
           <TableRow className="hover:bg-transparent">
-            <TableHead className="w-64">Name</TableHead>
-            <TableHead className="w-80">Email</TableHead>
-            <TableHead>Post Rewards</TableHead>
-            <TableHead>Chat Rewards</TableHead>
-            <TableHead>Total Rewarded</TableHead>
+            <TableHead className="">Name</TableHead>
+            <TableHead className="w-40">Total Rewarded</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {isLoading && <TableRowShimmer rows={filter.limit} colSpan={7} />}
 
-          {data && data.data.users?.length > 0 ? (
-            data.data.users.map((user) => (
-              <UserManagementRow key={user.id} user={user} handleToogleBlock={handleToogleBlock} />
-            ))
+          {data && data.rewards.length > 0 ? (
+            data.rewards.map((reward) => <UserReardTableRow key={reward.userId} reward={reward} />)
           ) : (
             <TableNoItemRow colSpan={7} message="No user found" />
           )}
@@ -53,11 +62,11 @@ const AdminRewardsPage = () => {
         </TableBody>
       </Table>
 
-      {data?.data && (
+      {data?.pagination && (
         <Pagination
           filter={filter}
           setFilter={setFilter}
-          totalPages={data.data.pagination?.totalPages}
+          totalPages={data.pagination?.totalPages}
         />
       )}
     </div>
